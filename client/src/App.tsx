621fcdd0913615298271614f6e30a5f17e53dc1c
Jürgen Efeish <jefeish@github.com>
2025-05-13 12:16:37 -0400

    modified layout(s)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -14,6 +14,7 @@ export const App: React.FC = () => {
       <div className="container mx-auto px-4 py-8">
         <Routes>
           <Route path="/" element={<Dashboard />} />
+          <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/migrations/:id" element={<MigrationDetails />} />
           <Route path="/settings" element={<Settings />} />
           <Route path="/reporting" element={<Reporting />} />
